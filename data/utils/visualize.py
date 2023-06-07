d671a18733922cc2d4b800a6cdc6dd4532eb88aa
Michael <michael@mshenoda.com>
2023-06-06 21:13:42 -0500

    Updated visualize.py and added distribution plots

--- data/utils/visualize.py
+++ data/utils/visualize.py
@@ -2,7 +2,7 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def visualize_class_distribution(csv_file, class_column):
+def visualize_class_distribution(csv_file, class_column, title):
     # Load the CSV dataset using pandas
     df = pd.read_csv(csv_file)
 
@@ -12,7 +12,7 @@ def visualize_class_distribution(csv_file, class_column):
     # Plot the class distribution
     plt.figure(figsize=(10, 6))
     class_counts.plot(kind='bar')
-    plt.title('Class Distribution')
+    plt.title(title) #'Class Distribution'
     plt.xlabel('Class')
     plt.ylabel('Count')
     plt.show()
@@ -22,9 +22,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Visualize class distribution in a CSV file.')
     parser.add_argument('csv_file', type=str, help='Path to the CSV file')
     parser.add_argument('class_column', type=str, help='Name of the column containing class labels')
+    parser.add_argument('title', type=str, help='Plot Title')
 
     # Parse command-line arguments
     args = parser.parse_args()
 
     # Call the visualization function
-    visualize_class_distribution(args.csv_file, args.class_column)
+    visualize_class_distribution(args.csv_file, args.class_column, args.title)
